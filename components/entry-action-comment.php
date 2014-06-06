192ac63c70ffb62b00ada5d835fb56598b87a5b9
Brian Sahagun <briansahagun@gmail.com>
2014-06-06 14:30:48 +0800

    Fixing via Theme Unit Test

--- components/entry-action-comment.php
+++ components/entry-action-comment.php
@@ -4,8 +4,8 @@
         <!-- Comment on Entry -->
         <li class="action-item action-comment action-comment-entry">
             <?php comments_popup_link( __( '<span class="comment-zero"><span class="label">Comment</span></span>', 'hopscotch' ),
-            __( '<span class="comment-one"><span class="comment-count">1</span> <span class="label">Comment</span></span>', 'hopscotch' ),
-            __( '<span class="comment-multiple"><span class="comment-count">%</span> <span class="label">Comments</span></span>', 'hopscotch' ),
+            __( '<span class="comment-one"><span class="counter comment-counter">1</span> <span class="label">Comment</span></span>', 'hopscotch' ),
+            __( '<span class="comment-multiple"><span class="counter comment-counter">%</span> <span class="label">Comments</span></span>', 'hopscotch' ),
             'action-link comment-entry-link', '<span class="comment-closed"><span class="label" data-state="inactive">Comments are closed.</span></span>' ); ?>
     
     </ul>
