70ce7b07bdbd4c7692c5815f99a8e4c57633c047
Brian Sahagun <briansahagun@gmail.com>
2014-06-18 20:20:10 +0800

    Finalizing nav

--- functions/post-id.php
+++ functions/post-id.php
@@ -6,6 +6,6 @@
 
 function hopscotch_custom_post_id() {
     global $post;
-    echo $post->post_name;
+    echo 'hopscotch-'.$post->post_name;
 }
 add_action('hopscotch_post_id','hopscotch_custom_post_id');
