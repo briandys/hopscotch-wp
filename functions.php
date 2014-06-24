70b379dceecef49379bee91bdaf02c41acd5bd47
Brian Sahagun <briansahagun@gmail.com>
2014-06-25 03:43:40 +0800

    Updates

--- functions.php
+++ functions.php
@@ -83,7 +83,7 @@ require_once( trailingslashit(get_template_directory()) . 'functions/auto-copyri
 require_once( trailingslashit(get_template_directory()) . 'functions/conditional-is-child.php');
 require_once( trailingslashit(get_template_directory()) . 'functions/post-count-format.php');
 require_once( trailingslashit(get_template_directory()) . 'functions/image-margin-fix.php');
-require_once( trailingslashit(get_template_directory()) . 'functions/entry-thumbnail-state.php');
+require_once( trailingslashit(get_template_directory()) . 'functions/entry-thumbnail-state-class.php');
 require_once( trailingslashit(get_template_directory()) . 'functions/page-id-via-slug.php');
 require_once( trailingslashit(get_template_directory()) . 'functions/svg-enable.php');
 
