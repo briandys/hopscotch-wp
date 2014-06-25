944a6ee6c9c367c35f219ff39147334aaa78b9e4
Brian Sahagun <briansahagun@gmail.com>
2014-06-25 21:21:37 +0800

    Updates

--- components/post-navigation.php
+++ components/post-navigation.php
@@ -1,15 +1,19 @@
 <nav class="action-items content-navigation post-navigation-action post-navigation" role="navigation">
     <h1 class="assistive-text"><?php _e( 'Page Navigation', 'hopscotch' ); ?></h1>
     <ul class="acton-list content-navigation-list post-navigation-list">
-        <?php if ( get_next_post_link() ) : ?>
-        <li class="action-item action-go-next">
-            <span class="label label-next">Next entry:</span> <?php next_post_link( '%link', _x( '%title', 'Next Post', 'hopscotch' ) ); ?>
-        </li>
-        <?php endif; ?>
-        <?php if ( get_previous_post_link() ) : ?>
+        
+        <?php // Previous
+        if ( get_previous_post_link() ) :
+        ?>
         <li class="action-item action-go-previous">
             <span class="label label-previous">Previous entry:</span> <?php previous_post_link( '%link', _x( '%title', 'Previous Post', 'hopscotch' ) ); ?>
-        </li>
+        <?php endif; ?>
+        
+        <?php // Next
+        if ( get_next_post_link() ) :
+        ?>
+        <li class="action-item action-go-next">
+            <span class="label label-next">Next entry:</span> <?php next_post_link( '%link', _x( '%title', 'Next Post', 'hopscotch' ) ); ?>
         <?php endif; ?>
     </ul>
 </nav><!-- action-items -->
