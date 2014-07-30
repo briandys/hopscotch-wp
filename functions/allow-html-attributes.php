64655fc2dcc2005c58d524c98771472cd19bbf58
Brian Dys Sahagun <briansahagun@gmail.com>
2014-07-30 11:08:37 +0800

    Allow New HTML Attributes in Dashboard

--- functions/allow-html-attributes.php
+++ functions/allow-html-attributes.php
@@ -5,16 +5,10 @@ function allow_all_tinymce_elements_attributes( $init ) {
 
     // Allow all elements and all attributes
     $ext = '*[*]';
-
-    // Add to extended_valid_elements if it already exists
-    if ( isset( $init['extended_valid_elements'] ) ) {
-        $init['valid_elements'] .= ',' . $ext;
-        $init['extended_valid_elements'] .= ',' . $ext;
-    } else {
-        $init['valid_elements'] = $ext;
-        $init['extended_valid_elements'] = $ext;
-    }
-
+    
+    $initArray['valid_elements'] = $ext;
+    $initArray['extended_valid_elements'] = $ext;
+    
     // return value
     return $init;
 }
