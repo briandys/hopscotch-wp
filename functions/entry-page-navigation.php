17d7db35d0be8339a9a82e120674b99bfc4a1fc0
Brian Dys Sahagun <briansahagun@gmail.com>
2014-08-07 21:24:21 +0800

    CSS updates

--- functions/entry-page-navigation.php
+++ functions/entry-page-navigation.php
@@ -5,7 +5,7 @@
 
 function hopscotch_wp_link_pages() {
     wp_link_pages( array(
-        'before'      => '<div class="action-items content-navigation entry-page-navigation page-links"><div class="content-navigation-cr"><div class="action-list content-navigation-list entry-page-navigation-list"><p class="text-label">' . __( 'Entry pages:', 'hopscotch' ) . '</p>',
+        'before'      => '<div class="action-items content-navigation entry-page-navigation page-links"><div class="content-navigation-cr"><div class="action-list content-navigation-list entry-page-navigation-list"><p class="text-label">' . __( 'Entry pages', 'hopscotch' ) . '</p>',
         'after'       => '</div></div></div>',
         'link_before' => '<span class="label">',
         'link_after'  => '</span>',
