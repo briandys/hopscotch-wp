c3e92d5565d0cbb0d91e0803fffea46688bf68a2
Brian Dys Sahagun <briansahagun@gmail.com>
2014-08-27 19:24:52 +0800

    Updates

--- functions/class-html.php
+++ functions/class-html.php
@@ -18,7 +18,7 @@ add_action( 'hopscotch_html_class', 'hopscotch_html_default_class');
 // This is the unique HTML class of site projects
 if ( ! function_exists( 'hopscotch_html_site_parent_class' ) ) :	
     function hopscotch_html_site_parent_class() {
-		echo 'site-default ';
+		echo 'site-default hopscotch--site ';
     }
     add_action( 'hopscotch_html_class', 'hopscotch_html_site_parent_class');
 endif;
