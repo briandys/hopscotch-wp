ba9246ebb48ee5ac81791e4526d836ce627cb018
Brian Dys Sahagun <briansahagun@gmail.com>
2014-09-01 19:31:53 +0800

    Updating theme file

--- functions/stylesheet.php
+++ functions/stylesheet.php
@@ -6,7 +6,7 @@ function hopscotch_styles() {
     wp_enqueue_style( 'hopscotch-style', get_template_directory_uri() . '/css/app.css', array(), '1.0', 'all' );
     
     if( is_child_theme() ) :
-        wp_enqueue_style( 'hopscotch-child-style', get_stylesheet_directory_uri() . '/css/app.css', array('hopscotch-style', 'hopscotch-fonts'), '1.0', 'all' );
+        wp_enqueue_style( 'hopscotch-child-style', get_stylesheet_directory_uri() . '/css/app.css', array('hopscotch-style', 'hopscotch-fonts'), '1.1', 'all' );
     endif;
 }
 add_action('wp_enqueue_scripts', 'hopscotch_styles');
