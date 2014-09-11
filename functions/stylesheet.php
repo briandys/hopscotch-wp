9f1fcf1a66fdc5e8114f7a1df8ae1afe0a0b4666
Brian Dys Sahagun <briansahagun@gmail.com>
2014-09-11 21:20:02 +0800

    Integrating Data UIs

--- functions/stylesheet.php
+++ functions/stylesheet.php
@@ -3,10 +3,10 @@
 //------------------------- CSS
 function hopscotch_styles() {
 	wp_enqueue_style( 'hopscotch-fonts' );
-    wp_enqueue_style( 'hopscotch-style', get_template_directory_uri() . '/css/app.css', array(), '1.0', 'all' );
+    wp_enqueue_style( 'hopscotch-style', get_template_directory_uri() . '/css/app.css', array(), '1.1', 'all' );
     
     if( is_child_theme() ) :
-        wp_enqueue_style( 'hopscotch-child-style', get_stylesheet_directory_uri() . '/css/app.css', array('hopscotch-style', 'hopscotch-fonts'), '1.1', 'all' );
+        wp_enqueue_style( 'hopscotch-child-style', get_stylesheet_directory_uri() . '/css/app.css', array('hopscotch-style', 'hopscotch-fonts'), '1.2', 'all' );
     endif;
 }
 add_action('wp_enqueue_scripts', 'hopscotch_styles');
