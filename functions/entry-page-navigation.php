aef06952ad09f416734f6e515f7bdbcbc23c386e
Brian Dys Sahagun <briansahagun@gmail.com>
2015-02-24 18:00:25 +0800

    Custom wp_link_pages

--- functions/entry-page-navigation.php
+++ functions/entry-page-navigation.php
@@ -2,12 +2,68 @@
 
 //------------------------- wp_link_pages
 // Displays the pages of a post (done by <!--nextpage-->)
+// http://codex.wordpress.org/Function_Reference/wp_link_pages
+// http://bavotasan.com/2012/a-better-wp_link_pages-for-wordpress
 
-function hopscotch_wp_link_pages() {
-    wp_link_pages( array(
-        'before'      => '<div class="action-items content-navigation entry-page-navigation page-links"><div class="content-navigation-cr"><div class="action-list content-navigation-list entry-page-navigation-list"><p class="text-label">' . __( 'Entry pages', 'hopscotch' ) . '</p>',
-        'after'       => '</div></div></div>',
-        'link_before' => '<span class="label">',
-        'link_after'  => '</span>',
-    ) );
+
+function hopscotch_wp_link_pages( $args = '' ) {
+	$defaults = array(
+		'before' => '<nav><h2 id="post-pagination">' . __( 'Entry Navigation' ) . '</h2><p class="friendly-name">' . __( 'Pages:' ) . '</p><ul>', 
+		'after' => '</ul></nav>',
+		'text_before' => '',
+		'text_after' => '',
+		'next_or_number' => 'number', 
+		'nextpagelink' => __( 'Next page' ),
+		'previouspagelink' => __( 'Previous page' ),
+		'pagelink' => '%',
+		'echo' => 1
+	);
+
+	$r = wp_parse_args( $args, $defaults );
+	$r = apply_filters( 'wp_link_pages_args', $r );
+	extract( $r, EXTR_SKIP );
+
+	global $page, $numpages, $multipage, $more, $pagenow;
+
+	$output = '';
+	if ( $multipage ) {
+		if ( 'number' == $next_or_number ) {
+			$output .= $before;
+			for ( $i = 1; $i < ( $numpages + 1 ); $i = $i + 1 ) {
+				$j = str_replace( '%', $i, $pagelink );
+				$output .= ' ';
+				if ( $i != $page || ( ( ! $more ) && ( $page == 1 ) ) )
+					$output .= '<li>' . _wp_link_page( $i );
+				else
+					$output .= '<li><span class="current-post-page">';
+
+				$output .= $text_before . $j . $text_after;
+				if ( $i != $page || ( ( ! $more ) && ( $page == 1 ) ) )
+					$output .= '</a>';
+				else
+					$output .= '</span>';
+			}
+			$output .= $after;
+		} else {
+			if ( $more ) {
+				$output .= $before;
+				$i = $page - 1;
+				if ( $i && $more ) {
+					$output .= _wp_link_page( $i );
+					$output .= $text_before . $previouspagelink . $text_after . '</a>';
+				}
+				$i = $page + 1;
+				if ( $i <= $numpages && $more ) {
+					$output .= _wp_link_page( $i );
+					$output .= $text_before . $nextpagelink . $text_after . '</a>';
+				}
+				$output .= $after;
+			}
+		}
+	}
+
+	if ( $echo )
+		echo $output;
+
+	return $output;
 }
